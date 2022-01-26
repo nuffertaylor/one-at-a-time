6c26fb43cc0cd3f6b4c339f8a42505e555d3f780
Taylor Nuffer <tnuffer@Taylors-MacBook-Air.local>
2022-01-26 15:01:33 -0700

    working product

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var inUse;
-var curUser;
+var inUse; //bool
+var curUser; //str
+var matchingCookie; //bool
 var inUseElement;
 var curUserElement;
 var startUsingButton;
@@ -16,9 +17,20 @@ function httpGet(theUrl)
 
 function initializeVars()
 {
-  console.log(httpGet("/inUse"));
-  inUse = false;
-  curUser = "";
+  let curCookie = parseCookie(document.cookie)["cookie"];
+  var res = JSON.parse(httpGet("/inUse"));
+  if(Object.keys(res).length === 0)
+  {
+    inUse = false;
+    curUser = "";
+    matchingCookie = false;
+  }
+  else
+  {
+    inUse = true;
+    curUser = res.name;
+    matchingCookie = (curCookie == res.cookie) ? true : false;
+  }
   inUseElement = document.getElementById("inUse");
   curUserElement = document.getElementById("curUser");
   startUsingButton = document.getElementById("startUsing");
@@ -26,19 +38,47 @@ function initializeVars()
   body = document.getElementById("body");
 }
 
+function genCookieStr(length)
+{
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) 
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  return result;
+}
+function writeTomorrowCookie(cookieStr)
+{
+  var now = new Date();
+  var time = now.getTime();
+  var expireTime = time + 1000*60*60*24;
+  now.setTime(expireTime);
+  document.cookie = "cookie=" + cookieStr + ";expires="+now.toUTCString() + ";path=/;";
+}
+const parseCookie = str => str.split(';').map(v => v.split('=')).reduce((acc, v) => {acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim()); return acc;}, {});
+function deleteCookie(){document.cookie = "cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";}
+
 function startUsing()
 {
   curUser = prompt("Please enter your name", "Harry Potter");
   if(curUser)
   {
+    let c = genCookieStr(9);
+    writeTomorrowCookie(c)
+    let registerUrl = "/startUsing?name=" + curUser + "&cookie=" + c;
+    httpGet(registerUrl);
     inUse = true;
+    matchingCookie = true;
     populatePage();
   }
 }
+
 function stopUsing()
 {
+  httpGet("/stopUsing");
+  deleteCookie();
   inUse = false;
   curUser = "";
+  matchingCookie = false;
   populatePage();
 }
 
@@ -60,7 +100,10 @@ function populatePage()
     inUseElement.innerHTML = "In Use";
     curUserElement.innerHTML = curUser + " is using it";
     startUsingButton.classList.add("hide");
-    stopUsingButton.classList.remove("hide");
+    if(matchingCookie)
+      stopUsingButton.classList.remove("hide");
+    else
+      stopUsingButton.classList.add("hide");
     body.classList.remove("availableColor");
     body.classList.add("inUseColor");
   }
--- server.py
+++ server.py
@@ -1,17 +1,62 @@
 import http.server
+import time
+import json
+import urllib
+from tinydb import TinyDB, Query
+db = TinyDB('db.json')
 
 def getCurrentUser():
-  return "johnny"
+  user = Query()
+  res = db.search(user.using == True)
+  if len(res) == 1:
+    return res[0]
+  elif len(res) > 1:
+    mostRecent = res[0]
+    for r in res:
+      if r["timestamp"] > mostRecent["timestamp"]:
+        mostRecent = r
+    return mostRecent
+  else:
+    return {}
 
 class Handler(http.server.SimpleHTTPRequestHandler) :
   def do_GET(s):
     if s.path == "/inUse":
-      msg = getCurrentUser()
+      msg = json.dumps(getCurrentUser())
       s.protocol_version = "HTTP/1.1"
       s.send_response(200)
       s.send_header("Content-Length", len(msg))
       s.end_headers()
       s.wfile.write(bytes(msg, "utf8"))
+
+    if "/startUsing" in s.path:
+      db.update({"using" : False}) #just make everyone else not use it
+      q = urllib.parse.urlparse(s.path).query
+      z = urllib.parse.parse_qsl(q)
+      name = ""
+      cookie = ""
+      for x in z:
+        if(x[0] == "name"):
+          name = x[1]
+        elif(x[0] == "cookie"):
+          cookie = x[1]
+      db.insert({"name" : name, "using" : True, "timestamp" : int(time.time()), "cookie" : cookie})
+      msg = json.dumps(getCurrentUser())
+      s.protocol_version = "HTTP/1.1"
+      s.send_response(200)
+      s.send_header("Content-Length", len(msg))
+      s.end_headers()
+      s.wfile.write(bytes(msg, "utf8"))
+
+    if s.path == "/stopUsing":
+      db.update({"using" : False})
+      msg = json.dumps(getCurrentUser())
+      s.protocol_version = "HTTP/1.1"
+      s.send_response(200)
+      s.send_header("Content-Length", len(msg))
+      s.end_headers()
+      s.wfile.write(bytes(msg, "utf8"))
+
     else:
       print('-----------------------')
       print('GET %s (from client %s)' % (s.path, s.client_address))
